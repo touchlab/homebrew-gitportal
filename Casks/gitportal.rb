c458e4fd7c8c0edb543b4d59eea23c26184f0f39
Kevin Galligan <kgalligan@gmail.com>
2024-04-15 22:57:00 -0400

    public s3 urls

--- Casks/gitportal.rb
+++ Casks/gitportal.rb
@@ -6,7 +6,7 @@ cask "gitportal" do
   sha256 arm:   "81eab945a33622fc156951e804024d23aa9a745c06743b4947215ed9303ad1c4",
          intel: "ede541af151487f60eb518e310d20dad1a973f3dbe9ff78d782dd29b14ba2946"
 
-  url "https://doppldocs.s3.amazonaws.com/gitportal-#{version}-#{arch}/",
+  url "https://doppldocs.s3.amazonaws.com/gitportal-#{version}-#{arch}",
       verified: "doppldocs.s3.amazonaws.com/"
 
   binary "gitportal-#{version}-#{arch}"
