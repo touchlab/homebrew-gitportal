d748ab0689533e98ac7b02e61acaf03072b29cfa
Kevin Galligan <kgalligan@gmail.com>
2024-04-16 16:39:33 -0400

    Build native

--- Formula/gitportal.rb
+++ Formula/gitportal.rb
@@ -2,31 +2,36 @@ class Gitportal < Formula
   desc "GitPortal CLI Tool"
   homepage "https://github.com/touchlab/GitPortal"
   url "https://github.com/touchlab/GitPortal.git",
-      tag:      "0.3.4",
-      revision: "396c67784b72539044acbd49a0596278bc4b855c"
+      tag:      "0.4.0",
+      revision: "5bc0ffe392123e841abd9bee0a5531be61e5e4ae"
   license "Apache-2.0"
   head "https://github.com/touchlab/GitPortal.git", branch: "main"
 
-  # bottle do
-  #   sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5bb02ca24ea211322ab73a829dd82056ab51045ce86241239a6bd8b6f7147b38"
-  #   sha256 cellar: :any_skip_relocation, arm64_ventura:  "10a1bef60f5bff99f07eac8f8f638ece20a70be8997d890779d2c1049f675a1a"
-  #   sha256 cellar: :any_skip_relocation, arm64_monterey: "9cb99bff69b8d12c35c30e5363bc39586cc502c05e76c25e8eebc87249c54ba3"
-  #   sha256 cellar: :any_skip_relocation, sonoma:         "65fd44b5c04141239380d8e0f203f0ddb99b63fce499879cffa512336bfd5111"
-  #   sha256 cellar: :any_skip_relocation, ventura:        "09121f1f8f89b5be892f2a04300d4368dcbd8b15a820291c3922bfeb846eaf96"
-  #   sha256 cellar: :any_skip_relocation, monterey:       "cfdb490e0cc99f0e9b0c447ac2e3987ce7d388a3a0a2ec5680c69f0d12666f6e"
-  # end
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5bb02ca24ea211322ab73a829dd82056ab51045ce86241239a6bd8b6f7147b38"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "10a1bef60f5bff99f07eac8f8f638ece20a70be8997d890779d2c1049f675a1a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9cb99bff69b8d12c35c30e5363bc39586cc502c05e76c25e8eebc87249c54ba3"
+    sha256 cellar: :any_skip_relocation, sonoma:         "65fd44b5c04141239380d8e0f203f0ddb99b63fce499879cffa512336bfd5111"
+    sha256 cellar: :any_skip_relocation, ventura:        "09121f1f8f89b5be892f2a04300d4368dcbd8b15a820291c3922bfeb846eaf96"
+    sha256 cellar: :any_skip_relocation, monterey:       "cfdb490e0cc99f0e9b0c447ac2e3987ce7d388a3a0a2ec5680c69f0d12666f6e"
+  end
 
   depends_on "gradle" => :build
-  depends_on cask: ["graalvm/tap/graalvm-jdk21", :build]
   depends_on xcode: ["14.1", :build]
   depends_on :macos
 
   def install
     suffix = (Hardware::CPU.arch == :x86_64) ? "X64" : "Arm64"
-    system "brew", "install", "--cask", "graalvm/tap/graalvm-jdk21"
-    system "gradle", "shadowJar"
-    system "native-image", "-jar", "core/build/libs/core-0.3-all.jar", "--no-fallback", "-H:+UnlockExperimentalVMOptions", "-H:ReflectionConfigurationFiles=core/reflectionconfig.json", "-H:+ReportUnsupportedElementsAtRuntime", "gitportal"
-    bin.install "gitportal" => "gitportal"
-    # share.install Dir["build/share/*"]
+    system "gradle", "--no-daemon", "linkReleaseExecutableMacos#{suffix}"
+    bin.install "build/bin/macos#{suffix}/releaseExecutable/cli.kexe" => "gitportal"
   end
+
+  # test do
+  #   output = shell_output(bin/"xcode-kotlin info --only")
+  #   assert_match "Bundled plugin version:\t\t#{version}", output
+  #   assert_match(/Installed plugin version:\s*(?:(?:\d+)\.(?:\d+)\.(?:\d+)|none)/, output)
+  #   assert_match(/Language spec installed:\s*(?:Yes|No)/, output)
+  #   assert_match(/LLDB init installed:\s*(?:Yes|No)/, output)
+  #   assert_match(/LLDB Xcode init sources main LLDB init:\s*(?:Yes|No)/, output)
+  # end
 end
