83a831311dc8dc54ef85dc702570e4cfbfeb6c8f
Kevin Galligan <kgalligan@gmail.com>
2024-05-22 04:35:24 +0000

    Add version 0.5.25

--- Formula/gitportal.rb
+++ Formula/gitportal.rb
@@ -2,8 +2,8 @@ class Gitportal < Formula
   desc "GitPortal CLI Tool"
   homepage "https://github.com/touchlab/GitPortalCli"
   url "https://github.com/touchlab/GitPortalCli.git",
-      tag: "0.5.24",
-      revision: "661d233d341605bf2d6cfb8077e74f0b02360b56"
+      tag: "0.5.25",
+      revision: "393d7e69039f561c582e5f407081cc9fce5ad674"
   license "Apache-2.0"
   head "https://github.com/touchlab/GitPortalCli.git", branch: "main"
 
