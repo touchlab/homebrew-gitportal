c542ef054d99b1bad437dbff7a162f69d5388518
Kevin Galligan <kgalligan@gmail.com>
2024-06-02 08:47:30 -0400

    Release Cadidate

---
+++ Formula/gitportal-rc.rb
@@ -0,0 +1,24 @@
+class GitportalRc < Formula
+  desc "GitPortal CLI Tool - Release Candidate"
+  homepage "https://github.com/touchlab/GitPortalCli"
+  url "https://github.com/touchlab/GitPortalCli.git",
+      tag: "0.5.28",
+      revision: "e3fedb8ce4c7eb68caa4afb9a333051a76dbedf9"
+  license "Apache-2.0"
+  head "https://github.com/touchlab/GitPortalCli.git", branch: "main"
+
+  depends_on "gradle" => :build
+  depends_on xcode: ["14.1", :build]
+  depends_on :macos
+
+  def install
+    suffix = (Hardware::CPU.arch == :x86_64) ? "X64" : "Arm64"
+    system "gradle", "--no-daemon", "linkReleaseExecutableMacos#{suffix}"
+    bin.install "cli-build/build/bin/macos#{suffix}/releaseExecutable/cli-build.kexe" => "gitportal"
+  end
+
+  test do
+    output = shell_output(bin/"gitportal-rc --help")
+    assert_match "Usage: gitportal options_list", output
+  end
+end
