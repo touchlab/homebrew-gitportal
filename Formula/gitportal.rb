b8fa8c4706a876846a0d53c3d0b5ba32f3267528
Kevin Galligan <kevin@touchlab.co>
2024-06-20 01:37:37 +0000

    gitportal: update 0.7.10 bottle.

--- Formula/gitportal.rb
+++ Formula/gitportal.rb
@@ -8,9 +8,9 @@ class Gitportal < Formula
   head "https://github.com/touchlab/GitPortalCli.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/touchlab/homebrew-gitportal/releases/download/gitportal-0.7.8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "85c0c1c3096e4111a22632a1bb5d8879b7631459d1f35fe8f45a496309efd0da"
-    sha256 cellar: :any_skip_relocation, ventura:      "cc0138d43de422a6ea16760653014adf8852bde1ec1d8979e60d654d8795067f"
+    root_url "https://github.com/touchlab/homebrew-gitportal/releases/download/gitportal-0.7.10"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "488fc97fe85048e9c7ee1a6728c1a05218698aa755fbf7893f24760132f55d5c"
+    sha256 cellar: :any_skip_relocation, ventura:      "64bc5b37b9b97e44d18cb9778175c992ee67e212334e59eaf0f6c6783ea27e85"
   end
 
   depends_on "gradle" => :build
